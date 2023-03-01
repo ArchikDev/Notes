8b1fcca6831c7e912947037eb1461fff635c5b96
a.galeev <a.galeev@malltech.ru>
2023-03-01 23:58:00 +0500

    add toolbar

--- app/src/main/java/com/archik/notes/MainActivity.kt
+++ app/src/main/java/com/archik/notes/MainActivity.kt
@@ -2,10 +2,43 @@ package com.archik.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.archik.notes.databinding.ActivityMainBinding
+import com.archik.notes.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var mToolbar: Toolbar
+  private lateinit var mNavController: NavController
+
+  private var _binding: ActivityMainBinding? = null
+  private val binding: ActivityMainBinding
+    get() = _binding ?: throw RuntimeException("ActivityMainBinding is null")
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    _binding = ActivityMainBinding.inflate(layoutInflater)
+
+    setContentView(binding.root)
+
+    APP_ACTIVITY = this
+
+    mToolbar = binding.toolbar
+
+    mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+    setSupportActionBar(mToolbar)
+
+    title = getString(R.string.title)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+
+    _binding = null
   }
 }
---
+++ app/src/main/java/com/archik/notes/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.archik.notes.utilits
+
+import com.archik.notes.MainActivity
+
+lateinit var APP_ACTIVITY:MainActivity

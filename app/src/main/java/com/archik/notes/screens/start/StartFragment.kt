e6c75e79e73b9c63f60b86757d5ff11e6351f97b
a.galeev <a.galeev@malltech.ru>
2023-03-03 09:59:41 +0500

    add database room

---
+++ app/src/main/java/com/archik/notes/screens/start/StartFragment.kt
@@ -0,0 +1,45 @@
+package com.archik.notes.screens.start
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.archik.notes.databinding.FragmentStartBinding
+import com.archik.notes.utilits.TYPE_ROOM
+
+class StartFragment : Fragment() {
+
+  private var _binding: FragmentStartBinding? = null
+  private val binding: FragmentStartBinding
+    get() = _binding ?: throw RuntimeException("FragmentStartBinding is null")
+
+  private lateinit var viewModel: StartFragmentViewModel
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+
+    _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
+
+    return binding.root
+  }
+
+  override fun onStart() {
+    super.onStart()
+
+    initialization()
+  }
+
+  private fun initialization() {
+    viewModel = ViewModelProvider(this)[StartFragmentViewModel::class.java]
+
+    with(binding) {
+      btnRoom.setOnClickListener {
+        viewModel.initDatabase(TYPE_ROOM)
+      }
+    }
+  }
+}
--- app/src/main/java/com/archik/notes/utilits/constants.kt
+++ app/src/main/java/com/archik/notes/utilits/constants.kt
@@ -3,3 +3,6 @@ package com.archik.notes.utilits
 import com.archik.notes.MainActivity
 
 lateinit var APP_ACTIVITY:MainActivity
+
+const val TYPE_DATABESE = "type_database"
+const val TYPE_ROOM = "type_room"
